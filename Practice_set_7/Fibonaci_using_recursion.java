ba0e6aa65ed30bfb6d630eeb6eebffe806ba0b38
Sanskar Wagavkar <sanskarwagavkar16@gmail.com>
2024-03-27 21:51:07 +0530

    code commit

---
+++ Practice_set_7/Fibonaci_using_recursion.java
@@ -0,0 +1,31 @@
+package Practice_set_7;
+
+import java.util.*;
+
+public class Fibonaci_using_recursion {
+
+    static void fibo(int n){
+        int a = 0;
+        int b = 1;
+
+        for(int i = 1; i <= n; i++){
+            System.out.println(a);
+            int c = a+b;
+            a = b;
+            b = c;
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number :- ");
+        int num = input.nextInt();
+        fibo(num);
+
+        input.close();
+
+    }
+}
