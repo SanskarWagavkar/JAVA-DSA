9cb20d276d6942ea5464f20a70e2e45a3e94f298
Sanskar Wagavkar <sanskarwagavkar16@gmail.com>
2024-03-30 02:36:44 +0530

    changes

---
+++ Practice_set_10/Inheritance_circle.java
@@ -0,0 +1,31 @@
+package Practice_set_10;
+
+
+class circle{
+    int radius;
+    double area(int radius){
+        this.radius = radius;
+        return 3.14*radius*radius;
+    }
+}
+class cyclinder extends circle{
+    int heigth;
+    double area(int radius, int heigth){
+        
+        this.radius = radius;
+        this.heigth = heigth;
+
+        return (2*3.14*radius*heigth)+(2*3.14*radius*radius);
+    }
+}
+
+
+
+public class Inheritance_circle {
+    public static void main(String[] args) {
+        circle c = new circle();
+        System.out.println(c.area(5));
+        cyclinder cl = new cyclinder();
+        System.out.println(cl.area(2,5));
+    }
+}
